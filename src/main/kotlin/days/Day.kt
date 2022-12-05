607d44554492976e95d01f6f85c468e16f7170e5
Thomas Poulain <thomas.poulain.partner@decathlon.com>
2022-12-06 00:08:39 +0100

    chore: add challenge url

--- src/main/kotlin/days/Day.kt
+++ src/main/kotlin/days/Day.kt
@@ -2,14 +2,13 @@ package days
 
 abstract class Day(private val day: Int, private val name: String) {
 
-    internal val input: List<String> =
-        Day::class.java.getResource("/day$day.txt")!!.readText().split("\n")
+    private val input: List<String> = Day::class.java.getResource("/day$day.txt")!!.readText().split("\n")
 
     abstract fun solvePart1(input: List<String>): String
     abstract fun solvePart2(input: List<String>): String
 
     fun solve() {
-        println("\n<Day $day - $name>")
+        println("\n<Day $day - $name> - https://adventofcode.com/2022/day/$day")
         val resultPartOne = solvePart1(input)
         println("Part One: $resultPartOne")
 
